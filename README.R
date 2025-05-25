0a846d74c22d437aa7f57dd453282f082e3676a3
mohamadpazh <141281966+mohamadpazh@users.noreply.github.com>
2025-05-25 13:21:33 +0100

    Update README.R

--- README.R
+++ README.R
@@ -23,8 +23,7 @@
         left: 0;
         width: 100%;
         height: 3px;
-      }
-      #nprogress .peg {
+      }    #nprogress .peg {
         display: block;
         position: absolute;
         right: 0px;
