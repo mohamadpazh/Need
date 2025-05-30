7d72ac78e0ab9f4d0f9a2f8128661234d54ebfee
mohamadpazh <141281966+mohamadpazh@users.noreply.github.com>
2025-05-30 12:03:14 +0100

    Update README.R

--- README.R
+++ README.R
@@ -15,9 +15,7 @@
         width: 100%;
         height: 3px;
       }
-
-
-      #nprogress .bar {
+   #nprogress .bar {
         background: #B1FF8F;
         position: fixed;
         z-index: 9999;
